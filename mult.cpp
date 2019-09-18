df7edb51d0b2f49d594e277515bf79a49ae38aeb
Juan Diego Castano <jcastanoma@unal.edu.co>
2019-09-18 05:05:35 -0400

    Se ha anhadido el archivo mult.cpp

---
+++ mult.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main (void)
+
+{
+  int num, mult;
+
+  cout << "Por favor, escriba un entero y pulse enter:";
+  cin>> num;
+  mult=num*2;
+  cout << "El entero multiplicado por 2 es: " <<mult <<endl;
+
+  return 0;
+}
+
+
