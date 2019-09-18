9898001cc4dc78571a4b9f41ecf774b1e972b42e
Juan Diego Castano <jcastanoma@unal.edu.co>
2019-09-18 09:52:17 -0400

    Se ha agregado el archivo hola.cpp

---
+++ hola.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main (void)
+{
+  string nombre;
+  cout << "Por favor, digite su nombre y pulse enter: ";
+  cin >> nombre;
+  cout << "Hola, " << nombre << "!" <<endl;
+  
+  return 0;
+}
